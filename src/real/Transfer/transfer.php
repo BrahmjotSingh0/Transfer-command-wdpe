1cfc81e8718200cbd36204becb7c9807ae202fb1
RealBJsingh <brahmjot05385@gmail.com>
2021-10-02 14:36:04 +0530

    ok useable now

--- src/real/Transfer/transfer.php
+++ src/real/Transfer/transfer.php
@@ -4,8 +4,9 @@ declare(strict_types=1);
 
 namespace real\Transfer;
 
-use pocketmine\command\Command;
-use pocketmine\command\CommandSender;
+use pocketmine\command\{Command, CommandSender};
+use pocketmine\utils\TextFormat as TF;
+use pocketmine\network\mcpe\NetworkSession;
 use pocketmine\command\utils\InvalidCommandSyntaxException;
 use pocketmine\network\mcpe\protocol\TransferPacket;
 use pocketmine\player\Player;
@@ -13,33 +14,31 @@ use function array_shift;
 use function count;
 
 class transfer extends PluginBase{
-
-	
-	public function onEnable(): void{
-        @mkdir($this->getDataFolder());
-        $this->saveResource("config.yml");
-        $this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML);
-    }
-
-  public function onCommand(CommandSender $sender, Command $cmd, String $label, Array $args) : bool{
-        
+    
+    public function onCommand(CommandSender $sender, Command $cmd, string $commandLabel, array $args): bool{
         switch($cmd->getName()){
             case "transfer":
-    $this->setPermission("wdpe.transfer.command");
-		$this->setUsage("/transfer <server>"); 
-		if(!$this->testPermission($sender)) return false;
-		if(!$sender instanceof Player) return false;
-		if(count($args) < 1) throw new InvalidCommandSyntaxException();
-		$server = array_shift($args);
-		$sender->sendMessage("§aTransferring you to {§server}");
-		$pk = new TransferPacket();
-		$pk->address = $server;
-		$pk->port = 1;
-		$sender->getNetworkSession()->sendDataPacket($pk);
-		return true;
-                } 
-            break;   
-        
-    } 
+                if(count($args) < 1){
+                    throw new InvalidCommandSyntaxException();
+                }
+                if($sender instanceof Player){
+                    if(!$sender->hasPermission("wdpe.transfer.command")){
+                        $sender->sendMessage("§cYou don't have permission to use this command!");
+                        return false;
+                    }
+                    $server = array_shift($args);
+                    $sender->sendMessage("§aTransferring you to {$server}");
+                    $pk = new TransferPacket();
+		            $pk->address = $server;
+		            $pk->port = 1;
+		            $sender->getNetworkSession()->sendDataPacket($pk);
+                }else{
+                    $sender->sendMessage("§crun this command in-game only!");
+                	return false;
+                }
+            break;
+        }
+        return true;
+    }
     
 }
